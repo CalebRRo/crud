0a676bcc5e63aa3c12c08a8fd77718fedef212b1
CalebRR <yohannagraciano@gmail.com>
2022-08-14 18:32:13 -0300

    modificando detail products

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -6,8 +6,11 @@ const controller = {
 	// Root - Show all products
 	index: (req, res) => {
 		// Do the magic
-		return res.render("index",
-		{products})
+		const products = loadProducts();
+		return res.render("products",{
+			products,
+			toThousand
+		});
 	},
 
 	// Detail - Detail from one product
@@ -24,11 +27,29 @@ const controller = {
 	// Create - Form to create
 	create: (req, res) => {
 		// Do the magic
+		return res.render("product-create-form")
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
 		// Do the magic
+		const {name,price,discount,description,category} = req.body
+		const products = loadProducts()
+
+		const newProduct = {
+			id : (products[products.length - 1].id + 1),
+			name : name.trim(),
+			description : description.trim(),
+			price : +price,
+			discount : +discount,
+			image : "default-image.png",
+			category
+		}
+
+		const productsModify = [...products, newProduct];
+		storeProducts(productsModify);
+
+		return res.redirect("/products");
 	},
 
 	// Update - Form to edit
@@ -43,7 +64,7 @@ const controller = {
 	// Update - Method to update
 	update: (req, res) => {
 		// Do the magic
-		const products = loadProducts()
+		const products = loadProducts();
 		const {name,price,discount,category,description} = req.body
 	    const productsModify = products.map(product => {
 			if (product.id === +req.params.id) {
@@ -58,7 +79,7 @@ const controller = {
 			}
 			return product
 		})
-		storeProducts(productsModify)
+		storeProducts(productsModify);
 		
 		return res.redirect("/products/detail/" + req.params.id)
 	},
@@ -66,6 +87,11 @@ const controller = {
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
 		// Do the magic
+		const {id} = req.params;
+		const products = loadProducts();
+		const productsModify = products.filter(product => product.id !== +id);
+		storeProducts(productsModify);
+		return res.redirect("/products");
 	}
 };
 
